b8fb4a6e52d71808a428a7c1e664d538fa925583
Andiichi <andrezza.n.dasilva@gmail.com>
2024-09-20 16:04:42 -0300

    update do arquivo content.jsx na linha 86

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -83,9 +83,9 @@ export function Content() {
                 <details>
                   <summary className={styles.cardRepoSummary}>
                     {repo.nome}
+                    <q className={styles.cardRepoQuote}>{repo.citacao}</q>
                   </summary>
                   <p className={styles.cardRepoText}>{repo.minibio}</p>
-                  <q className={styles.cardRepoQuote}>{repo.citacao}</q>
                 </details>
               </div>
               )
